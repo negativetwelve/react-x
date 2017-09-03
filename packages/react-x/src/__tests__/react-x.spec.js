c5c7213acb314c0d97bf9835b841816f6b0d1e0f
Mark Miyashita <negativetwelve@gmail.com>
2017-09-02 23:54:19 -0700

    Add generic tests to react-x to make sure we're exporting components properly

--- packages/react-x/src/__tests__/react-x.spec.js
+++ packages/react-x/src/__tests__/react-x.spec.js
@@ -5,13 +5,20 @@ import ReactX from '../react-x';
 
 /* eslint-disable no-undef */
 describe('ReactX', () => {
-  describe('Switch', () => {
+  const components = [
+    'Render',
+    'Switch',
+  ];
+
+  forEach({component: components}, () => {
     it('should exist', () => {
-      expect(ReactX.Switch).toBeDefined();
+      expect(ReactX[component]).toBeDefined();
     });
 
+    // TODO(mark): Create generic matcher to support stateless and class-based
+    // components.
     it('should be a component', () => {
-      expect(ReactX.Switch).toBeInstanceOf(Function);
+      expect(ReactX[component]).toBeInstanceOf(Function);
     });
   });
 });
