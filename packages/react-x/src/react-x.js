5dd295fb4762ceef4dc906edc1d721ec5cbcc2a7
Mark Miyashita <negativetwelve@gmail.com>
2017-09-02 19:22:31 -0700

    Create single entry point for react-x

---
+++ packages/react-x/src/react-x.js
@@ -0,0 +1,8 @@
+/* eslint-disable brace-style */
+const ReactX = {
+  get Switch() { return require('react-x-switch'); },
+};
+
+
+// Supports importing a single object or using named imports.
+module.exports = ReactX;
