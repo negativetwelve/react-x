fd85e2e42203b425d5c5c402d8edf12edf12ae69
Mark Miyashita <negativetwelve@gmail.com>
2017-09-02 23:41:14 -0700

    Add quick snapshot tests for the Render component

--- packages/react-x-render/src/__tests__/Render.spec.js
+++ packages/react-x-render/src/__tests__/Render.spec.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Render from '../Render';
 
 
+const Text = ({children}) => <div>{children}</div>;
+
 /* eslint-disable no-undef */
 describe('Render', () => {
   context('with default props', () => {
     action('render', () => (
       <Render>
-        {'Something rendered'}
+        <Text>{'Something rendered'}</Text>
       </Render>
     ));
 
@@ -19,21 +21,37 @@ describe('Render', () => {
     set('ifCondition', () => false);
     action('render', () => (
       <Render if={ifCondition}>
-        {'Something rendered'}
+        <Text>{'Something rendered'}</Text>
       </Render>
     ));
 
-    itRenders(() => render());
+    context('with ifCondition set to true', () => {
+      set('ifCondition', () => true);
+      itRenders(() => render());
+    });
+
+    context('with ifCondition set to false', () => {
+      set('ifCondition', () => false);
+      itRenders(() => render());
+    });
   });
 
   context('with unless prop', () => {
     set('unlessCondition', () => true);
     action('render', () => (
       <Render unless={unlessCondition}>
-        {'Something rendered'}
+        <Text>{'Something rendered'}</Text>
       </Render>
     ));
 
-    itRenders(() => render());
+    context('with unlessCondition set to true', () => {
+      set('unlessCondition', () => true);
+      itRenders(() => render());
+    });
+
+    context('with unlessCondition set to false', () => {
+      set('unlessCondition', () => false);
+      itRenders(() => render());
+    });
   });
 });
