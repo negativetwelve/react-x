3ce399d01e991d5d7607b3d8d85d478fcb803227
Mark Miyashita <negativetwelve@gmail.com>
2017-09-02 23:23:10 -0700

    Create 'Render' component for react-x-render-conditional

---
+++ packages/react-x-render-conditional/src/Render.js
@@ -0,0 +1,25 @@
+// Libraries
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Render = ({if: ifCondition, unless, children}) => {
+  if (ifCondition && !unless && children) {
+    return React.Children.only(children);
+  } else {
+    return null;
+  }
+};
+
+Render.propTypes = {
+  if: PropTypes.bool,
+  unless: PropTypes.bool,
+};
+
+Render.defaultProps = {
+  if: true,
+  unless: false,
+};
+
+
+export default Render;
