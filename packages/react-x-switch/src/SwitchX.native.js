8cec7a5f57afc0c3f7789b237a58fd2419446b3d
Mark Miyashita <negativetwelve@gmail.com>
2017-09-02 17:09:37 -0700

    Implement the SwitchX for native which uses ReactNative.Switch

--- packages/react-x-switch/src/SwitchX.native.js
+++ packages/react-x-switch/src/SwitchX.native.js
@@ -4,54 +4,31 @@ import ReactNative from 'react-native';
 import PropTypes from 'prop-types';
 
 
-class Switch extends Component {
+const SwitchX = (props) => (
+  <ReactNative.Switch
+    disabled={props.isDisabled}
+    value={props.isOn}
+    tintColor={props.tintColor}
+    onTintColor={props.onTintColor}
+    thumbTintColor={props.thumbTintColor}
+    onValueChange={props.onChange}
+  />
+);
 
-  // --------------------------------------------------
-  // Props
-  // --------------------------------------------------
-  static propTypes = {
-    isOn: PropTypes.bool.isRequired,
-    onChange: PropTypes.func,
-    onTintColor: PropTypes.color,
-    thumbTintColor: PropTypes.color,
-    tintColor: PropTypes.color,
-    isDisabled: PropTypes.bool,
-  };
+// --------------------------------------------------
+// Props
+// --------------------------------------------------
+SwitchX.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  tintColor: PropTypes.string.isRequired,
+  onTintColor: PropTypes.string.isRequired,
+  thumbTintColor: PropTypes.string.isRequired,
+  isOn: PropTypes.bool.isRequired,
+  isDisabled: PropTypes.bool.isRequired,
+};
 
-  static defaultProps = {
-    onChange: () => {},
-    onTintColor: Colors.quad.blue,
-    thumbTintColor: null,
-    tintColor: null,
-    isDisabled: false,
-  };
-
-  // --------------------------------------------------
-  // Render
-  // --------------------------------------------------
-  render() {
-    const {isDisabled, isOn, onChange} = this.props;
-    const {tintColor, onTintColor, thumbTintColor} = this.props;
-
-    return (
-      <ReactNative.Switch
-        disabled={isDisabled}
-        value={isOn}
-        tintColor={tintColor}
-        onTintColor={onTintColor}
-        thumbTintColor={thumbTintColor}
-        onValueChange={onChange}
-      />
-    );
-  }
-
-  // --------------------------------------------------
-  // Styles
-  // --------------------------------------------------
-  static styles = {
-  };
-
-}
+SwitchX.defaultProps = {
+};
 
 
-export default Switch;
+export default SwitchX;
