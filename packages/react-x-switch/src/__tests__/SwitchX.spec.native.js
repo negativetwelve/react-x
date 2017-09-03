aef871a3d1761df9fca980b6b821c9fc7f0abdf1
Mark Miyashita <negativetwelve@gmail.com>
2017-09-02 17:48:24 -0700

    Add all required props for SwitchX.native

--- packages/react-x-switch/src/__tests__/SwitchX.spec.native.js
+++ packages/react-x-switch/src/__tests__/SwitchX.spec.native.js
@@ -5,9 +5,18 @@ import SwitchX from '../SwitchX';
 
 /* eslint-disable no-undef */
 describe('SwitchX.native', () => {
+  set('onTintColor', () => '#000000');
+  set('isDisabled', () => false);
+
   context('with default props', () => {
     forEach({isOn: [true, false]}, () => {
-      itRenders(() => <SwitchX isOn={isOn} />);
+      itRenders(() => (
+        <SwitchX
+          isOn={isOn}
+          isDisabled={isDisabled}
+          onTintColor={onTintColor}
+        />
+      ));
     });
   });
 });
