0c82626c77dcac92f925fd47b841e8b762c93c1e
Mark Miyashita <negativetwelve@gmail.com>
2017-09-04 05:30:56 -0700

    Add initialize tests to the Keychain module

--- packages/react-x-keychain/src/__tests__/Keychain.spec.js
+++ packages/react-x-keychain/src/__tests__/Keychain.spec.js
@@ -4,9 +4,8 @@ import Keychain from '../Keychain';
 
 /* eslint-disable no-undef */
 describe('Keychain', () => {
-  set('namespace', () => undefined);
-
   describe('constructor', () => {
+    set('namespace', () => undefined);
     action('construct', () => new Keychain({namespace}));
 
     context('with namespace', () => {
@@ -26,7 +25,39 @@ describe('Keychain', () => {
     });
   });
 
-  describe('initialize', () => {
-    // TODO
+  context('with keychain instance', () => {
+    set('namespace', () => 'com.somecompany');
+    set('keychain', () => new Keychain({namespace}));
+
+    describe('initialize', () => {
+      set('keys', () => []);
+      action('initialize', () => keychain.initialize(keys));
+
+      context('with no keys', () => {
+        it('should resolve to undefined', () => {
+          expect(initialize()).resolves.toEqual(undefined);
+        });
+      });
+
+      context('with keys', () => {
+        set('keys', () => ['my', 'key', 'chain']);
+
+        context('with empty keychain', () => {
+          it('should resolve to undefined', () => {
+            expect(initialize()).resolves.toEqual(undefined);
+          });
+
+          context('with primed cache', () => {
+            beforeEach(async () => initialize());
+
+            forEach({key: ['my', 'key', 'chain']}, () => {
+              it('should set key to undefined', () => {
+                expect(keychain.get(key)).toEqual(undefined);
+              });
+            });
+          });
+        });
+      });
+    });
   });
 });
