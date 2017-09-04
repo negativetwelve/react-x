89293b68468cc727f1a20b41bfa6709d0c8ce842
Mark Miyashita <negativetwelve@gmail.com>
2017-09-04 06:02:32 -0700

    Add mocks for the window since we can't quite use jsdom yet

--- test/jest-config.dom.js
+++ test/jest-config.dom.js
@@ -6,6 +6,10 @@ module.exports = {
     '\\.(css|less|sass)$': 'identity-obj-proxy',
   },
   setupTestFrameworkScriptFile: '<rootDir>/test/setup.dom.js',
+
+  // TODO(mark): This should be 'jsdom' except enzyme is requiring react-dom < 16.
+  // This causes everything to fail. Tracking issue is:
+  // https://github.com/airbnb/enzyme/issues/928
   testEnvironment: 'node',
   testMatch: ['**/?(*.)spec.js', '**/?(*.)spec.dom.js'],
 };
--- test/setup.dom.js
+++ test/setup.dom.js
@@ -1,2 +1,9 @@
 // react-dom specific mocks go here.
 import './setup';
+
+// TODO(mark): See the jest-config.dom.js testEnvironment comment for why
+// this is necessary.
+global.window = {
+  document: {},
+  addEventListener: () => {},
+};
