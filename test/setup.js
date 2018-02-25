f9f7e1e031d6f37a7bd79443cc6f89e4fc9a919b
Mark Miyashita <negativetwelve@gmail.com>
2018-02-24 21:55:24 -0800

    Add hack for react-x-primitives tests and add snapshots

--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,8 @@
 // Jolt
 import 'jest-preset-jolt/setup';
 import 'jest-plugins-react/setup';
+
+// Hack for react-art referencing the document object.
+global.document = {
+  createElement: () => null,
+};
