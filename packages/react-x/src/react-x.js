005c8a31e53feddc7662b51b455f3cc0837d9f80
Mark Miyashita <negativetwelve@gmail.com>
2018-02-25 00:11:59 -0800

    Export react-x-primitives and react-x-styled components and apis

--- packages/react-x/src/react-x.js
+++ packages/react-x/src/react-x.js
@@ -1,12 +1,21 @@
 /* eslint-disable brace-style */
 const ReactX = {
   // Components
+  get Image() { return require('react-x-primitives').Image; },
+  get SVG() { return require('react-x-primitives').SVG; },
+  get Text() { return require('react-x-primitives').Text; },
+  get View() { return require('react-x-primitives').View; },
+
   get Render() { return require('react-x-render').default; },
   get Switch() { return require('react-x-switch').default; },
 
   // APIs
+  get Animated() { return require('react-x-primitives').Animated; },
+  get StyleSheet() { return require('react-x-primitives').StyleSheet; },
+
   get Clipboard() { return require('react-x-clipboard').default; },
   get Keychain() { return require('react-x-keychain').default; },
+  get Styled() { return require('react-x-styled').default; },
 };
 
 
