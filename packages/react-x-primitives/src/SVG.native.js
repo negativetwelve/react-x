429beb18711b68e20e2a06eb73c9653df12391bd
Mark Miyashita <negativetwelve@gmail.com>
2018-02-24 23:05:22 -0800

    Set exact versions for react-x-primitives

--- packages/react-x-primitives/src/SVG.native.js
+++ packages/react-x-primitives/src/SVG.native.js
@@ -1,3 +1,4 @@
-// NOTE(mark): The ios/android implementations are the same for the svg library.
+// NOTE(mark): The ios/android implementations are the same for the
+// react-primitives-svg library.
 import * as SVG from 'react-primitives-svg/lib/core.ios';
 export default SVG;
