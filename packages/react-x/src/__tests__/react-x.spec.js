585bd8c65741b8f21b88d4a003ee71d93679c4bb
Mark Miyashita <negativetwelve@gmail.com>
2018-02-26 16:46:06 -0800

    [react-x-components] Create the initial react-x-components package + migrate everything over (#56)
    * Create the react-x-components package and move all component exports there
    
    * Add tests for react-x-components
    
    * Remove individual components from the react-x export

--- packages/react-x/src/__tests__/react-x.spec.js
+++ packages/react-x/src/__tests__/react-x.spec.js
@@ -2,43 +2,20 @@
 import React from 'react';
 import ReactX from '../react-x';
 
-
 /* eslint-disable no-undef */
 describe('ReactX', () => {
-  const components = [
-    // TODO(mark): Can't test SVG because it is a set of components.
-    'Image',
-    'Text',
-    'View',
-
-    'Render',
-    'Switch',
-  ];
-
-  forEach({component: components}, () => {
-    it('should exist', () => {
-      expect(ReactX[component]).toBeDefined();
-    });
-
-    // TODO(mark): Create generic matcher to support stateless and class-based
-    // components.
-    it('should be a component', () => {
-      expect(ReactX[component]).toBeInstanceOf(Function);
-    });
-  });
-
-  const apis = [
+  const names = [
     'Animated',
-    'StyleSheet',
-
     'Clipboard',
+    'Components',
     'Keychain',
     'Styled',
+    'StyleSheet',
   ];
 
-  forEach({api: apis}, () => {
+  forEach({name: names}, () => {
     it('should exist', () => {
-      expect(ReactX[api]).toBeDefined();
+      expect(ReactX[name]).toBeDefined();
     });
   });
 });
