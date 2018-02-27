3457959f421250fef2fa1de85435e992e08bf690
Mark Miyashita <negativetwelve@gmail.com>
2018-02-26 16:31:35 -0800

    Create the react-x-components package and move all component exports there

---
+++ packages/react-x-components/src/Components.js
@@ -0,0 +1,9 @@
+/* eslint-disable brace-style */
+module.exports = {
+  get Image() { return require('react-x-primitives').Image; },
+  get Render() { return require('react-x-render').default; },
+  get SVG() { return require('react-x-primitives').SVG; },
+  get Switch() { return require('react-x-switch').default; },
+  get Text() { return require('react-x-primitives').Text; },
+  get View() { return require('react-x-primitives').View; },
+};
--- packages/react-x/src/react-x.js
+++ packages/react-x/src/react-x.js
@@ -1,23 +1,9 @@
 /* eslint-disable brace-style */
-const ReactX = {
-  // Components
-  get Image() { return require('react-x-primitives').Image; },
-  get SVG() { return require('react-x-primitives').SVG; },
-  get Text() { return require('react-x-primitives').Text; },
-  get View() { return require('react-x-primitives').View; },
-
-  get Render() { return require('react-x-render').default; },
-  get Switch() { return require('react-x-switch').default; },
-
-  // APIs
+module.exports = {
   get Animated() { return require('react-x-primitives').Animated; },
-  get StyleSheet() { return require('react-x-primitives').StyleSheet; },
-
   get Clipboard() { return require('react-x-clipboard').default; },
+  get Components() { return require('react-x-components'); },
   get Keychain() { return require('react-x-keychain').default; },
   get Styled() { return require('react-x-styled').default; },
+  get StyleSheet() { return require('react-x-primitives').StyleSheet; },
 };
-
-
-// Supports importing a single object or using named imports.
-module.exports = ReactX;
