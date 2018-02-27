68a67f30ca70536bf1145cb1f6f21d1a3a65609d
Mark Miyashita <negativetwelve@gmail.com>
2018-02-26 17:02:00 -0800

    Add star export for the react-x-svg library

--- packages/react-x-svg/src/SVG.dom.js
+++ packages/react-x-svg/src/SVG.dom.js
@@ -1,2 +1,7 @@
 import * as SVG from 'react-primitives-svg/lib/core.web';
+
+// Export the SVG object as the default.
 export default SVG;
+
+// Export all individual components.
+export * from 'react-primitives-svg/lib/core.web';
--- packages/react-x-svg/src/SVG.native.js
+++ packages/react-x-svg/src/SVG.native.js
@@ -1,4 +1,9 @@
 // NOTE(mark): The ios/android implementations are the same for the
 // react-primitives-svg library.
 import * as SVG from 'react-primitives-svg/lib/core.ios';
+
+// Export the SVG object as the default.
 export default SVG;
+
+// Export all individual components.
+export * from 'react-primitives-svg/lib/core.ios';
