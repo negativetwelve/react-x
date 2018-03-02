5da4352248b707513c5dadaca1ee6f3059a83540
Mark Miyashita <negativetwelve@gmail.com>
2018-03-02 00:47:53 -0800

    [react-x] Export all components from the main react-x object (#59)
    * Remove the react-x-components package and move exports to react-x
    
    * Remove the Components export from react-x tests

--- packages/react-x/src/__tests__/react-x.spec.js
+++ packages/react-x/src/__tests__/react-x.spec.js
@@ -4,18 +4,40 @@ import ReactX from '../react-x';
 
 /* eslint-disable no-undef */
 describe('ReactX', () => {
-  const names = [
-    'Animated',
-    'Clipboard',
-    'Components',
-    'Keychain',
-    'Styled',
-    'StyleSheet',
-  ];
+  context('APIs', () => {
+    const apis = [
+      'Animated',
+      'Clipboard',
+      'Keychain',
+      'Styled',
+      'StyleSheet',
+    ];
 
-  forEach({name: names}, () => {
-    it('should exist', () => {
-      expect(ReactX[name]).toBeDefined();
+    forEach({api: apis}, () => {
+      it('should exist', () => {
+        expect(ReactX[api]).toBeDefined();
+      });
+    });
+  });
+
+  context('Components', () => {
+    const components = [
+      // TODO(mark): Can't test SVG because it is a set of components.
+      'Image',
+      'Render',
+      'Switch',
+      'Text',
+      'View',
+    ];
+
+    forEach({component: components}, () => {
+      it('should exist', () => {
+        expect(ReactX[component]).toBeDefined();
+      });
+
+      it('should be a component', () => {
+        expect(ReactX[component]).toBeInstanceOf(Function);
+      });
     });
   });
 });
--- packages/react-x/src/react-x.js
+++ packages/react-x/src/react-x.js
@@ -1,9 +1,17 @@
 /* eslint-disable brace-style */
 module.exports = {
+  // APIs
   get Animated() { return require('react-x-primitives').Animated; },
   get Clipboard() { return require('react-x-clipboard').default; },
-  get Components() { return require('react-x-components'); },
   get Keychain() { return require('react-x-keychain').default; },
   get Styled() { return require('react-x-styled').default; },
   get StyleSheet() { return require('react-x-primitives').StyleSheet; },
+
+  // Components
+  get Image() { return require('react-x-primitives').Image; },
+  get Render() { return require('react-x-render').default; },
+  get SVG() { return require('react-x-svg').default; },
+  get Switch() { return require('react-x-switch').default; },
+  get Text() { return require('react-x-primitives').Text; },
+  get View() { return require('react-x-primitives').View; },
 };
