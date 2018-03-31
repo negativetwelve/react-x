90e09f523a8acf0f3a32c91c2feefbeb49b65b2e
Mark Miyashita <negativetwelve@gmail.com>
2018-03-31 01:22:37 -0700

    [react-x-text-input] Create the package for react-x-text-input component (#69)
    * Create separate package for the react-x-text-input
    
    * Add tests for the react-x-text-input component

---
+++ packages/react-x-text-input/src/__tests__/TextInput.spec.dom.js
@@ -0,0 +1,10 @@
+// Modules
+import React from 'react';
+import TextInput from '../TextInput';
+
+/* eslint-disable no-undef */
+describe('TextInput', () => {
+  context('with no props', () => {
+    itRenders(() => <TextInput />);
+  });
+});
