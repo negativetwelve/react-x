981663bcbf1c6e7f7472d24534f6c20f8916c6e4
Mark Miyashita <negativetwelve@gmail.com>
2018-03-30 23:00:00 -0700

    Package up jest-preset for native into a separate package

---
+++ packages/jest-preset-react-x-native/setup.js
@@ -0,0 +1,24 @@
+// Jolt
+require('jest-preset-jolt/setup');
+require('jest-plugins-react/setup');
+
+// Hack for react-art referencing the document object.
+global.document = {
+  createElement: () => null,
+};
+
+// Need to mock react-native. react-native-mock doesn't work with 0.47+.
+jest.mock('react-native', () => ({
+  // TODO(mark): Proper component mocks are much needed and all snapshots
+  // will need to be updated once this mock is complete.
+
+  Switch: () => null, // react-x-switch
+  NativeModules: {}, // react-x-keychain
+}));
+
+// Mock out react-native-svg since we don't have the native components.
+jest.mock('react-native-svg', () => ({
+  Circle: () => null,
+  Line: () => null,
+  Rect: () => null,
+}));
--- test/jest-config.native.js
+++ test/jest-config.native.js
@@ -1,14 +1,4 @@
 module.exports = {
   rootDir: '../',
-  preset: 'jest-preset-jolt',
-  moduleFileExtensions: ['js', 'native.js'],
-  setupTestFrameworkScriptFile: '<rootDir>/test/setup.native.js',
-  testEnvironment: 'node',
-  testMatch: ['**/?(*.)spec.js', '**/?(*.)spec.native.js'],
-  transformIgnorePatterns: [
-    // NOTE(mark): React Native and all packages must be compiled by babel in
-    // order to work properly. This issue is being tracked:
-    // https://github.com/facebook/react-native/issues/10966
-    '/node_modules/(?!react-native|react-native-keychain)',
-  ],
+  preset: 'jest-preset-react-x-native',
 };
