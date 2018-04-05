df21407a6c93713c8bd8257a723b179466c903dd
Mark Miyashita <negativetwelve@gmail.com>
2018-04-05 00:14:58 -0700

    Specify that the jest mocks are virtual for react-native and react-native-svg

--- packages/jest-preset-react-x-native/setup.js
+++ packages/jest-preset-react-x-native/setup.js
@@ -9,6 +9,7 @@ global.document = {
 };
 
 // Need to mock react-native. react-native-mock doesn't work with 0.47+.
+// Specifying `virtual: true` will mock out this module only if it exists.
 jest.mock('react-native', () => ({
   // TODO(mark): Proper component mocks are much needed and all snapshots
   // will need to be updated once this mock is complete.
@@ -16,11 +17,11 @@ jest.mock('react-native', () => ({
   Switch: () => null, // react-x-switch
   TextInput: () => null, // react-x-text-input
   NativeModules: {}, // react-x-keychain
-}));
+}), {virtual: true});
 
 // Mock out react-native-svg since we don't have the native components.
 jest.mock('react-native-svg', () => ({
   Circle: () => null,
   Line: () => null,
   Rect: () => null,
-}));
+}), {virtual: true});
