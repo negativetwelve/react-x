9ec2362623d75d02b3734057513d73d30fe2f7dc
Mark Miyashita <negativetwelve@gmail.com>
2019-05-10 19:28:22 -0700

    Create babel config package

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',
-  ],
-  plugins: [
-    '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-transform-flow-strip-types',
-    '@babel/plugin-transform-runtime',
+    'module:babel-preset-react-x',
   ],
 };
