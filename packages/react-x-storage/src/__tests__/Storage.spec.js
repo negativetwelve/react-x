1ef6c0792aff808340233b23ae556b7b82797732
Mark Miyashita <negativetwelve@gmail.com>
2019-05-10 18:43:13 -0700

    Add tests for react-x-storage

---
+++ packages/react-x-storage/src/__tests__/Storage.spec.js
@@ -0,0 +1,36 @@
+// Modules
+import Storage from '../Storage';
+
+
+/* eslint-disable no-undef */
+describe('Storage', () => {
+  describe('getItem', () => {
+    set('key', () => 'KEY');
+    action('getItem', () => Storage.getItem(key));
+
+    context('with empty storage', () => {
+      it('should return undefined', () => {
+        expect(getItem()).resolves.toEqual(undefined);
+      });
+    });
+
+    context('with existing value', () => {
+      set('value', () => 'VALUE');
+      beforeEach(() => Storage.setItem(key, value));
+
+      it('should return the value', () => {
+        expect(getItem()).resolves.toEqual(value);
+      });
+    });
+  });
+
+  describe('setItem', () => {
+    set('key', () => 'KEY');
+    set('value', () => 'VALUE');
+    action('setItem', () => Storage.setItem(key, value));
+
+    context('with empty storage', () => {
+      // TODO
+    });
+  });
+});
