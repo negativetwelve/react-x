8a8728dac427f5c6557b0e957929b0ae9f5814bf
Mark Miyashita <negativetwelve@gmail.com>
2019-05-10 19:19:16 -0700

    Update snapshots and use babel 7 config

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-transform-flow-strip-types',
+    '@babel/plugin-transform-runtime',
+  ],
+};
