3fdea95c2a71ae4511ccdb901e6f5034edddb912
Mark Miyashita <negativetwelve@gmail.com>
2019-05-27 19:41:50 -0700

    [jest-preset-react-x-dom] Add actual browser mock and createPortal mock (#87)
    * Add browser and createPortal mock to the jest dom package
    
    * Add browser-env mock package
    
    * Update dom snapshot tests

--- packages/jest-preset-react-x-dom/setup.js
+++ packages/jest-preset-react-x-dom/setup.js
@@ -3,37 +3,5 @@ require('jest-plugins')([
   'jest-plugins-recommended',
 ]);
 
-// TODO(mark): Replace this with a proper window/document/element setup.
-const element = {
-  setAttribute: () => null,
-};
-
-const location = {
-  href: undefined,
-};
-
-const navigator = {
-};
-
-const screen = {
-  height: undefined,
-  width: undefined,
-};
-
-const document = {
-  addEventListener: () => {},
-  createElement: () => element,
-  getElementById: () => element,
-};
-
-const window = {
-  addEventListener: () => {},
-  document,
-  location,
-  navigator,
-  screen,
-};
-
-global.document = document;
-global.navigator = navigator;
-global.window = window;
+require('./src/browser');
+require('./src/react-dom');
