1ce6c1fe801e5fb27100c09ca815ed4445448745
Mark Miyashita <negativetwelve@gmail.com>
2019-05-27 18:29:57 -0700

    Add better mock window, document, and navigator components

--- packages/jest-preset-react-x-dom/setup.js
+++ packages/jest-preset-react-x-dom/setup.js
@@ -3,14 +3,37 @@ require('jest-plugins')([
   'jest-plugins-recommended',
 ]);
 
-// Hack for react-art referencing the document object.
-global.document = {
-  createElement: () => null,
+// TODO(mark): Replace this with a proper window/document/element setup.
+const element = {
+  setAttribute: () => null,
 };
 
-// TODO(mark): See the jest-config.dom.js testEnvironment comment for why
-// this is necessary.
-global.window = {
-  document: global.document,
-  addEventListener: () => {},
+const location = {
+  href: undefined,
 };
+
+const navigator = {
+};
+
+const screen = {
+  height: undefined,
+  width: undefined,
+};
+
+const document = {
+  addEventListener: () => {},
+  createElement: () => element,
+  getElementById: () => element,
+};
+
+const window = {
+  addEventListener: () => {},
+  document,
+  location,
+  navigator,
+  screen,
+};
+
+global.document = document;
+global.navigator = navigator;
+global.window = window;
